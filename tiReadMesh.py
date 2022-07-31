259f2ee98379f38cb01bcc4b67c99a3a3acf2fb6
chunleili <48758868+chunleili@users.noreply.github.com>
2022-07-31 19:40:20 +0800

    Update tiReadMesh.py

--- tiReadMesh.py
+++ tiReadMesh.py
@@ -8,22 +8,22 @@ numEdges = len(bunnyMesh['tetEdgeIds']) // 2
 numTets = len(bunnyMesh['tetIds']) // 4
 numSurfs = len(bunnyMesh['tetSurfaceTriIds']) // 3
 
-verts = np.array(bunnyMesh['verts'], dtype=float)
-tetIds = np.array(bunnyMesh['tetIds'], dtype=int)
-tetEdgeIds = np.array(bunnyMesh['tetEdgeIds'], dtype=int)
-tetSurfaceTriIds = np.array(bunnyMesh['tetSurfaceTriIds'], dtype=int)
+pos_np = np.array(bunnyMesh['verts'], dtype=float)
+tet_np = np.array(bunnyMesh['tetIds'], dtype=int)
+edge_np = np.array(bunnyMesh['tetEdgeIds'], dtype=int)
+surf_np = np.array(bunnyMesh['tetSurfaceTriIds'], dtype=int)
 
-verts = verts.reshape((-1,3))
-tetIds = tetIds.reshape((-1,4))
-tetEdgeIds = tetEdgeIds.reshape((-1,2))
-tetSurfaceTriIds = tetSurfaceTriIds.reshape((-1,3))
+pos_np = pos_np.reshape((-1,3))
+tet_np = tet_np.reshape((-1,4))
+edge_np = edge_np.reshape((-1,2))
+surf_np = surf_np.reshape((-1,3))
 
 pos = ti.Vector.field(3, float, numParticles)
 tet = ti.Vector.field(4, int, numTets)
 edge = ti.Vector.field(2, int, numEdges)
 surf = ti.Vector.field(3, int, numSurfs)
 
-pos.from_numpy(verts)
-tet.from_numpy(tetIds)
-edge.from_numpy(tetEdgeIds)
-surf.from_numpy(tetSurfaceTriIds)
+pos.from_numpy(pos_np)
+tet.from_numpy(tet_np)
+edge.from_numpy(edge_np)
+surf.from_numpy(surf_np)
